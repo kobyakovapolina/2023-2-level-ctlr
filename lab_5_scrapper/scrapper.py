bee4d9b0c0078eb120025a120b7f2c600b81a4ec
kobyakova polina <polinagg.pk@gmail.com>
2024-04-22 20:44:39 +0300

    mark 6

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -7,11 +7,13 @@ from typing import Pattern, Union
 import requests
 import re
 import json
-from random import randrange
-from time import sleep
-from bs4 import BeautifulSoup
-from core_utils.article.article import Article
+import random
+import time
+import datetime
 
+from bs4 import BeautifulSoup
+from core_utils.article.io import to_raw, to_meta
+from core_utils.article.article import Article
 from core_utils import constants
 from core_utils.config_dto import ConfigDTO
 
@@ -34,6 +36,7 @@ class IncorrectTimeoutError(Exception):
 
 class IncorrectVerifyError(Exception):
     pass
+
 
 class Config:
     """
@@ -68,14 +71,14 @@ class Config:
         """
         with open(self.path_to_config, 'r', encoding='utf-8') as f:
             config = json.load(f)
-        return ConfigDTO(
-            seed_urls=config['seed_urls'],
-            total_articles_to_find_and_parse=config['total_articles_to_find_and_parse'],
-            headers=config['headers'],
-            encoding=config['encoding'],
-            timeout=config['timeout'],
-            should_verify_certificate=config['should_verify_certificate'],
-            headless_mode=config['headless_mode'])
+        return ConfigDTO(**config)
+            #seed_urls=config['seed_urls'],
+            #total_articles_to_find_and_parse=config['total_articles_to_find_and_parse'],
+            #headers=config['headers'],
+            #encoding=config['encoding'],
+            #timeout=config['timeout'],
+            #should_verify_certificate=config['should_verify_certificate'],
+            #headless_mode=config['headless_mode'])
 
     def _validate_config_content(self) -> None:
         """
@@ -156,10 +159,10 @@ class Config:
 
     def get_verify_certificate(self) -> bool:
         """
-        Retrieve number of seconds to wait for response.
+        Retrieve whether to verify certificate.
 
         Returns:
-            int: Number of seconds to wait for response
+            bool: Whether to verify certificate or not
         """
         return self._should_verify_certificate
 
@@ -184,8 +187,14 @@ def make_request(url: str, config: Config) -> requests.models.Response:
     Returns:
         requests.models.Response: A response from a request
     """
-    return requests.get(url=url, timeout=config.get_timeout(), headers=config.get_headers(),
-                        verify=config.get_verify_certificate())
+    period = random.randrange(10)
+    time.sleep(period)
+
+    response = requests.get(url=url, timeout=config.get_timeout(),
+                            headers=config.get_headers(),
+                            verify=config.get_verify_certificate())
+
+    return response
 
 
 class Crawler:
@@ -204,7 +213,7 @@ class Crawler:
         """
         self.urls = []
         self.config = config
-        self.url = "https://elementy.ru"
+        self.url_pattern = 'https://elementy.ru/novosti_nauki'
 
     def _extract_url(self, article_bs: BeautifulSoup) -> str:
         """
@@ -216,27 +225,47 @@ class Crawler:
         Returns:
             str: Url from HTML
         """
+        #url = ''
+        #links = article_bs.find_all('a', class_="nohover")
+        #for link in links:
+        #    url = link.get('href')
+        #url = self.url_pattern + url
+        #return url
 
         url = ''
-        for div in article_bs.find_all("div", class_="title", limit=10):
-            for link in div.select("a"):
-                url = link.get("href")
+        links = article_bs.find_all('a', class_="nohover")
+        for link in links:
+            url = link.get('href')
+            if url not in self.urls:
+                break
+        url = self.url_pattern + url[len("/novosti_nauki")::]
         return url
 
     def find_articles(self) -> None:
         """
         Find articles.
         """
-        for seed_url in self.get_search_urls():
+        seed_urls = self.get_search_urls()
+        for seed_url in seed_urls:
             response = make_request(seed_url, self.config)
 
             if not response.ok:
                 continue
 
+            #article_bs = BeautifulSoup(response.text, "html.parser")
+            #urls = [self._extract_url(article_bs) for i in range(10)]
+            #self.urls.extend(urls)
+
             article_bs = BeautifulSoup(response.text, "html.parser")
             article_url = self._extract_url(article_bs)
-            if article_url:
+            while article_url:
+                if len(self.urls) == self.config.get_num_articles():
+                    break
                 self.urls.append(article_url)
+                article_url = self._extract_url(article_bs)
+
+            if len(self.urls) == self.config.get_num_articles():
+                break
 
     def get_search_urls(self) -> list:
         """
@@ -265,6 +294,10 @@ class HTMLParser:
             article_id (int): Article id
             config (Config): Configuration
         """
+        self.full_url = full_url
+        self.article_id = article_id
+        self.config = config
+        self.article = Article(self.full_url, self.article_id)
 
     def _fill_article_with_text(self, article_soup: BeautifulSoup) -> None:
         """
@@ -274,6 +307,45 @@ class HTMLParser:
             article_soup (bs4.BeautifulSoup): BeautifulSoup instance
         """
 
+        #article = ''
+        #article_texts = article_soup.find(class_="memo")
+        #texts = article_texts.find_all('p')
+        #for text in texts:
+        #    article += text.text + '\n'
+        #self.article.text = article
+
+        #print(self.article.text)
+
+        for clas in article_soup.find_all(class_='small'):
+            clas.decompose()
+
+        text = article_soup.find(class_="memo")
+        full_text = ''
+        all_p_tags = text.find_all("p", recursive=False)[:-1]
+
+        for p in all_p_tags:
+            full_text += p.text + '\n'
+        self.article.text = full_text
+
+        #raw_text = ''
+        #article_texts = article_soup.find(class_="memo")
+        #text_blocks = article_texts.find_all('p')
+        #for text_block in text_blocks:
+        #    if text_block.string:
+        #        raw_text += f'\n{text_block.string}'
+#
+        #self.article.text = raw_text
+        #print(raw_text)
+
+        #all_div = article_soup.find(class_="memo")
+        #full_text = ''
+        #all_p_tags = all_div.find_all("p", recursive=False)[:-1]
+#
+        #for p in all_p_tags:
+        #    full_text += p.text + '\n'
+        #self.article.text = full_text
+        #print(self.article.text)
+
     def _fill_article_with_meta_information(self, article_soup: BeautifulSoup) -> None:
         """
         Find meta information of article.
@@ -281,8 +353,33 @@ class HTMLParser:
         Args:
             article_soup (bs4.BeautifulSoup): BeautifulSoup instance
         """
+        self.article.title = str(article_soup.find(class_='title').string)
+        print(self.article.title)
 
-    #def unify_date_format(self, date_str: str) -> datetime.datetime:
+        author = article_soup.find_all('p')[-1]
+        author = author.get_text()
+        if author:
+            self.article.author = [author]
+        else:
+            self.article.author = ["NOT FOUND"]
+        print(self.article.author)
+
+        #author = str(article_soup.find('i', class_="small"))
+        #if author:
+        #    self.article.author = [author]
+        #else:
+        #    self.article.author = ["NOT FOUND"]
+#
+        #print(self.article.author)
+
+        #author = article_soup.find_all('i', class_='small')[-1]
+        #print(author)
+
+        date = article_soup.find('span', class_='date')
+        self.article.date = date
+        print(self.article.date)
+
+    def unify_date_format(self, date_str: str) -> datetime.datetime:
         """
         Unify date format.
 
@@ -300,6 +397,13 @@ class HTMLParser:
         Returns:
             Union[Article, bool, list]: Article instance
         """
+        response = make_request(self.full_url, self.config)
+        if response.ok:
+            article_bs = BeautifulSoup(response.text, 'html.parser')
+            self._fill_article_with_text(article_bs)
+            self._fill_article_with_meta_information(article_bs)
+
+        return self.article
 
 
 def prepare_environment(base_path: Union[pathlib.Path, str]) -> None:
@@ -309,7 +413,10 @@ def prepare_environment(base_path: Union[pathlib.Path, str]) -> None:
     Args:
         base_path (Union[pathlib.Path, str]): Path where articles stores
     """
+    base_path.mkdir(parents=True, exist_ok=True)
 
+    for file in base_path.iterdir():
+        file.unlink(missing_ok=True)
 
 def main() -> None:
     """
@@ -318,8 +425,14 @@ def main() -> None:
     configuration = Config(path_to_config=constants.CRAWLER_CONFIG_PATH)
     crawler = Crawler(configuration)
     base_path = constants.ASSETS_PATH
-    crawler.find_articles()
     prepare_environment(base_path)
+    crawler.find_articles()
+
+    for i, full_url in enumerate(crawler.get_search_urls()):
+        parser = HTMLParser(full_url=full_url, article_id=i, config=configuration)
+        article = parser.parse()
+        to_raw(article)
+        to_meta(article)
 
 
 if __name__ == "__main__":
